3a330af2d00163267efdb50b8d3ab90de92d1890
kurodakeita <167636389+kurodakeita@users.noreply.github.com>
2024-07-25 21:19:20 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+      
+      $user_names = ['name'=>'onion','price'=>'200','weight'=>'160'];
+      print_r($user_names)
+ 
+         ?>
+     </p>
+ </body>
+ 
+ </html>
